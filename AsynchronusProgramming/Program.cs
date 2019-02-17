1dbf9959505015ab5d0ffcd44b8fb31d336d7518
Shree <shreesks99@gmail.com>
2019-02-17 11:39:20 +0000

    Add project files.

---
+++ AsynchronusProgramming/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsynchronusProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Downloadig the file");
+            Download();
+            Console.ReadLine();
+       
+
+        }
+
+        private static async void Download()
+        {
+            HttpClient client = new HttpClient();
+            var data =await client.GetAsync("https://www.udemy.com/join/login-popup/?next=/home/my-courses/learning/");
+            Console.WriteLine("Download COmpleted "+ data);
+        }
+    }
+}
