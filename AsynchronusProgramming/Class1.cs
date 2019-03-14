d80e63e3ee08b401506176b8d5f40e71b57074aa
Srinivas Singardi <outsourcer.sks@big.pt>
2019-03-14 15:54:35 +0000

    add

---
+++ AsynchronusProgramming/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsynchronusProgramming
+{
+    class Class1
+    {
+        //added code here 
+        public int add(int a , int b, int c)
+        {
+            var sum = a+b;
+            return sum;
+
+            // some more chages to the class 
+            
+            //added soem comments
+        }
+        
+        public void sub()
+        {
+        //int a; int b ;
+            int c  = a-b;
+        }
+        
+        //one more method frome the other person
+        
+        public void mul()
+        {
+        int a ; 
+            int b ;
+        }
+    }
+}
