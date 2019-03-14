93e7b3e8fbff2efe85b908ca36a6f3717f788857
Srinivas Singardi <outsourcer.sks@big.pt>
2019-03-14 16:26:21 +0000

    add one clsss with the mehtod ADD()

---
+++ AsynchronusProgramming/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsynchronusProgramming
+{
+    class Class1
+    {
+        public void add ()
+        {
+            int a = 4;
+            int b = 58;
+            int c = a + b; 
+
+
+        }
+    }
+}
